ca0c333536b8cc70dd336b8ef71b58daf9ef9f9d
Cromato <107234074+Cromato@users.noreply.github.com>
2022-06-10 02:44:56 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import math as math
 import pandas as pd
 import numpy as np
 
+
 def prob_n_land(n_land, cards_mao, lands, cards_tot): #probabilidade de se ter exatamente n lands na mão, se a mão tiver c cartas e l lands no deck
     res1=1
     res2=1
@@ -46,11 +47,17 @@ def prob_fin(lands_min,lands_max,n_mulligan,lands, cards_tot, primeiro_free): #p
         else :
             res = res + (1-res)*res
             for i in range (2,n_mulligan+1):
-                res = res+(1-res)*prob_range_land(lands_min,lands_max,cards_mao-(i-1), lands, cards_tot)
+                res_prod = 1
+                for j in range (1,i):
+                    res_prod=res_prod*(1-prob_range_land(lands_min,lands_max,cards_mao-i+j+1, lands, cards_tot))
+                res = res+(1-prob_range_land(lands_min,lands_max,cards_mao, lands, cards_tot))*res_prod*prob_range_land(lands_min,lands_max,cards_mao-(i-1), lands, cards_tot)
 
     else :
-        for i in range (0,n_mulligan):
-            res=res+(1-res)*prob_range_land(lands_min,lands_max,cards_mao-(i+1), lands, cards_tot)
+        for i in range (1,n_mulligan+1):
+            res_prod = 1
+            for j in range (0,i):
+                res_prod=res_prod*(1-prob_range_land(lands_min,lands_max,cards_mao-i+j+1, lands, cards_tot))
+            res=res+res_prod*prob_range_land(lands_min,lands_max,cards_mao-i, lands, cards_tot)
 
     return res
 
@@ -64,10 +71,13 @@ def mulligan_fixo(lands_min,lands_max,n_mulligan, cards_tot, primeiro_free):
         x.append(i) #eixo x
         y.append(prob_fin(lands_min,lands_max,n_mulligan,lands, cards_tot, primeiro_free)) #eixo y
         delta.append(y[i]-y[i-1]) # taxa de variação da probabilidade
-    top_jumps_val=sorted(delta)[-20:] #valores dos 3 maiores saltos
+    top_jumps_val=sorted(delta)[-20:] #valores dos 20 maiores saltos
     for j in range(0,20):
         top_jumps_pos.append(delta.index(top_jumps_val[19-j])+1) #posição dos 3 maiores saltos
     return x,y, top_jumps_pos
+
+
+#plt.show(), print(f'Biggest jumps in probability happen at these thresholds, in order: {top_jumps_pos}')
 
 
 cards_mao = 7 #cards na sua mão - inicia como 7
@@ -86,6 +96,7 @@ if radio_box == "Info":
     st.caption("                  -- 3.0.0 - Beauty in presentation")
     st.markdown("This is the web version of the mana base calculator. It gives you the probabilities of begining the game with a set number of lands in your hand.")
     st.markdown("The current version has a problem, that for probabilities near 1, some values get round up to 0, so that they don't drop as they should afterwards. This has to be corrected by increasing the precision of the calculations.")
+    st.markdown("You can check the source code of the application at https://github.com/Cromato/ManaBase.")
 
     st.header("How the probabilities are beeing computed")
     st.markdown("The first thing to do is to compute the probability of having exactly $n\_land$ lands in your hand, $cards\_mao$ cards in your hand and $lands$ lands in your deck of $cards\_tot$ total cards. This is given by the probability of a specific combination of $n\_land$ lands, times the number of configurations with $n\_land$. This means")
@@ -115,10 +126,36 @@ if radio_box == "Info":
     st.latex("P(range|n\_mulligan)=P(range)")
     st.latex("+\displaystyle\sum_{i=1}^{n\_mulligan}\displaystyle\prod_{j=0}^{i-1}(1-P(range|cards\_mao-i+j+1))\cdot P(range|cards\_mao-i)")
     st.markdown("and if the firs Mulligan is free:")
-    st.latex("P(lands\_min\leq x\leq lands\_max|n\_mulligan)=P(lands\_min\leq x\leq lands\_max)+(1-P(lands\_min\leq x\leq lands\_max))P(lands\_min\leq x\leq lands\_max)")
-    st.latex("+\displaystyle\sum_{i=2}^{n\_mulligan}(1-P(lands\_min\leq x\leq lands\_max|cards\_mao-i+2))\cdot P(lands\_min\leq x\leq lands\_max|cards\_mao-i+1)")
+    st.latex("P(range|n\_mulligan)=P(range)+(1-P(range))P(range)")
+    st.latex("+\displaystyle\sum_{i=2}^{n\_mulligan}\prod_{j=1}^{i-1}(1-P(range))(1-P(range|cards\_mao-i+j+1))\cdot P(range|cards\_mao-i+1)")
+    st.markdown("The code used for this function is:")
+    st.code('''def prob_fin(lands_min,lands_max,n_mulligan,lands, cards_tot, primeiro_free):
+    res=prob_range_land(lands_min,lands_max,cards_mao, lands, cards_tot)
+    
+    if primeiro_free == True:
+        if n_mulligan == 0:
+            res = res
+            
+        elif n_mulligan == 1:
+            
+            res = res + (1-res)*res
+            
+        else :
+            res = res + (1-res)*res
+            for i in range (2,n_mulligan+1):
+                res_prod = 1
+                for j in range (1,i):
+                    res_prod=res_prod*(1-prob_range_land(lands_min,lands_max,cards_mao-i+j+1, lands, cards_tot))
+                res = res+(1-prob_range_land(lands_min,lands_max,cards_mao, lands, cards_tot))*res_prod*prob_range_land(lands_min,lands_max,cards_mao-(i-1), lands, cards_tot)
 
+    else :
+        for i in range (1,n_mulligan+1):
+            res_prod = 1
+            for j in range (0,i):
+                res_prod=res_prod*(1-prob_range_land(lands_min,lands_max,cards_mao-i+j+1, lands, cards_tot))
+            res=res+res_prod*prob_range_land(lands_min,lands_max,cards_mao-i, lands, cards_tot)
 
+    return res''')
 
 
 if radio_box == "App":
